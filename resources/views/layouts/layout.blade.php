869f358b377d595a1063a4af582297551d018c4f
Pro.Orator <Pro.Orator@gmail.com>
2020-06-22 12:14:48 +0300

    make pagination and searc

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Блог</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <div class="container collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="col-6 navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Главная <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active offset-3">
+                <a class="nav-link" href="{{ route('post.create') }}">Создать пост <span class="sr-only">(current)</span></a>
+            </li>
+
+        </ul>
+        <form class="form-inline my-2 my-lg-0" action="{{ route('post.index') }}">
+            <input class="form-control mr-sm-2" name="search" type="search" placeholder="Найти пост..." aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Поиск</button>
+        </form>
+    </div>
+</nav>
+
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PostController@index');
+Route::get('post/', 'PostController@index')->name('post.index');
+Route::get('post/create', 'PostController@create')->name('post.create');
 
